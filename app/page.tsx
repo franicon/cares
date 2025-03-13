f547ab7d3a111d30b0fcbdb40d2724d7b7976bb6
frani <franicons@gmail.com>
2025-03-13 03:26:01 +0100

    forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import PatientForm from "@/components/forms/PatientForm";
+import {SearchParamProps} from "@/types";
+import {PasskeyModel} from "@/components/PasskeyModel";
 
-export default function Home() {
+export default function Home({searchParams}: SearchParamProps) {
+    const isAdmin = searchParams.admin === "true";
     return (
         <div className="flex h-screen max-h-screen">
-            {/*Todo OTP verification | passkeyModel*/}
+            {isAdmin && <PasskeyModel/>}
             <section className="remove-scrollbar container my-auto">
                 <div className="sub-container max-w-[496px]">
                     <Image src="/assets/icons/logo-full.svg"
